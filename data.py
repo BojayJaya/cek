706dabcd1cd894669ba4d26da8f82b850b43673c
BojayJaya <95891943+BojayJaya@users.noreply.github.com>
2023-06-15 10:00:31 +0700

    Update data.py

--- data.py
+++ data.py
@@ -11,14 +11,14 @@ from sklearn.tree import DecisionTreeClassifier
 st.title("Penambangan Data")
 st.write("### Dosen Pengampu: Mula'ab, S.Si., M.Kom.")
 st.write("##### Kelompok")
-st.write("##### Fadetul Fitriyeh- 200411100189")
+st.write("##### Fadetul Fitriyeh - 200411100189")
 st.write("##### R.Bella Aprilia Damayanti - 200411100082")
 st.write('Dataset yang digunakan yaitu dataset XL AXIATA yang diambil dari yahoo.com')
 
 data_set_description, upload_data, preprocessing, modeling, implementation = st.tabs(["Data Set Description", "Upload Data", "Preprocessing", "Modeling", "Implementation"])
 
 with data_set_description:
-    st.write("""# Data Set Description """)
+    st.write("# Data Set Description ")
     st.write("###### Source Code Aplikasi ada di Github anda bisa acces di link :  https://github.com/RBellaApriliaDamayanti22/uas")
     st.write("###### Untuk Wa saya anda bisa hubungi nomer ini : http://wa.me/6289658567766 ")
 
@@ -27,11 +27,11 @@ with upload_data:
     st.dataframe(df)
 
 with preprocessing:
-    st.subheader("""Normalisasi Data""")
-    st.write("""Rumus Normalisasi Data :""")
+    st.subheader("Normalisasi Data")
+    st.write("Rumus Normalisasi Data :")
     st.image('https://i.stack.imgur.com/EuitP.png', use_column_width=False, width=250)
     st.markdown("""
-    Dimana :
+    Dimana:
     - X = data yang akan dinormalisasi atau data asli
     - min = nilai minimum semua data asli
     - max = nilai maksimum semua data asli
@@ -50,13 +50,12 @@ with preprocessing:
 with modeling:
     training, test, training_label, test_label = train_test_split(scaled_df, y, test_size=0.2, random_state=1)
 
-    with st.form("modeling"):
-        st.subheader('Modeling')
-        st.write("Pilihlah model yang akan dilakukan pengecekkan akurasi:")
+    st.subheader('Modeling')
+    model_selection = st.checkbox('Pilih Model untuk Pengecekan Akurasi')
+    if model_selection:
         naive = st.checkbox('Gaussian Naive Bayes')
         k_nn = st.checkbox('K-Nearest Neighbors')
         destree = st.checkbox('Decision Tree')
-        submitted = st.form_submit_button("Submit")
 
         if naive:
             gaussian = GaussianNB()
@@ -81,31 +80,30 @@ with modeling:
             st.write("Model Decision Tree accuracy score:", dt_accuracy)
 
 with implementation:
-    with st.form("my_form"):
-        st.subheader("Implementasi")
-        volume = st.number_input('Volume:')
-        model = st.selectbox('Pilih model untuk prediksi:',
-                            ('Gaussian Naive Bayes', 'K-Nearest Neighbors', 'Decision Tree'))
-        submitted = st.form_submit_button("Submit")
+    st.subheader("Implementasi")
+    volume = st.number_input('Volume:')
+    model = st.selectbox('Pilih model untuk prediksi:',
+                        ('Gaussian Naive Bayes', 'K-Nearest Neighbors', 'Decision Tree'))
+    submitted = st.button("Submit")
 
-        if submitted:
-            input_data = np.array([[volume]])
-            scaler = MinMaxScaler()
-            scaled_input = scaler.fit_transform(input_data)
+    if submitted:
+        input_data = np.array([[volume]])
+        scaler = MinMaxScaler()
+        scaled_input = scaler.fit_transform(input_data)
 
-            if model == 'Gaussian Naive Bayes':
-                mod = GaussianNB()
-                mod.fit(training, training_label)
-            elif model == 'K-Nearest Neighbors':
-                K = 10
-                mod = KNeighborsClassifier(n_neighbors=K)
-                mod.fit(training, training_label)
-            elif model == 'Decision Tree':
-                mod = DecisionTreeClassifier()
-                mod.fit(training, training_label)
+        if model == 'Gaussian Naive Bayes':
+            mod = GaussianNB()
+            mod.fit(training, training_label)
+        elif model == 'K-Nearest Neighbors':
+            K = 10
+            mod = KNeighborsClassifier(n_neighbors=K)
+            mod.fit(training, training_label)
+        elif model == 'Decision Tree':
+            mod = DecisionTreeClassifier()
+            mod.fit(training, training_label)
 
-            input_pred = mod.predict(scaled_input)
+        input_pred = mod.predict(scaled_input)
 
-            st.subheader('Hasil Prediksi')
-            st.write('Menggunakan Model:', model)
-            st.write('Volume:', input_pred[0])
+        st.subheader('Hasil Prediksi')
+        st.write('Menggunakan Model:', model)
+        st.write('Volume:', input_pred[0])
