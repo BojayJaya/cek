f5044d3c9269dcc2d3ebe6c83d46436f19b1f539
BojayJaya <95891943+BojayJaya@users.noreply.github.com>
2023-06-15 09:18:54 +0700

    Update data.py

--- data.py
+++ data.py
@@ -1,118 +1,180 @@
 import streamlit as st
-
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.ensemble import StackingClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import GaussianNB
-from sklearn import metrics
-from sklearn.naive_bayes import GaussianNB
-from sklearn.model_selection import train_test_split
+from PIL import Image
+import altair as alt
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+# import warnings
+# warnings.filterwarnings("ignore")
 
-import pickle
 
-from sklearn import metrics
+st.title("Penambangan Data ")
+st.write("### Dosen Pengampu : Mula'ab, S.Si., M.Kom.")
+st.write("##### Kelompok ")
+st.write("##### Fadetul Fitriyeh- 200411100189 ")
+st.write("##### R.Bella Aprilia Damayanti - 200411100082")
+st.write('Dataset yang digunakan yaitu dataset XL AXIATA yang diambil dari yahoo.com')
 
-st.set_page_config(
-    page_title="Project"
-)
+data_set_description, upload_data, preprocessing, modeling, implementation = st.tabs(["Data Set Description", "Upload Data", "Preprocessing", "Modeling", "Implementation"])
 
-tab1, tab2, tab3 = st.tabs(["Dataset","Prepocessing","Implementation"])
+with data_set_description:
+    st.write("""# Data Set Description """)
+    st.write("###### Source Code Aplikasi ada di Github anda bisa acces di link :  https://github.com/RBellaApriliaDamayanti22/uas")
+    st.write("###### Untuk Wa saya anda bisa hubungi nomer ini : http://wa.me/6289658567766 ")
 
-with tab1:
-    st.title("Penambangan Data ")
-    st.write("### Dosen Pengampu : Mula'ab, S.Si., M.Kom.")
-    st.write("##### Kelompok ")
-    st.write("##### Fadetul Fitriyeh- 200411100189 ")
-    st.write("##### R.Bella Aprilia Damayanti - 200411100082")
-    st.write('Dataset yang digunakan yaitu dataset XL AXIATA yang diambil dari yahoo.com')
+with upload_data:
+    # uploaded_files = st.file_uploader("Upload file CSV", accept_multiple_files=True)
+    # for uploaded_file in uploaded_files:
+    #     df = pd.read_csv(uploaded_file)
+    #     st.write("Nama File Anda = ", uploaded_file.name)
+    #     st.dataframe(df)
+    df = pd.read_csv('https://raw.githubusercontent.com/RBellaApriliaDamayanti22/Datasets/main/XL-axiata.csv')
+    st.dataframe(df)
+
+with preprocessing:
+    st.subheader("""Normalisasi Data""")
+    st.write("""Rumus Normalisasi Data :""")
+    st.image('https://i.stack.imgur.com/EuitP.png', use_column_width=False, width=250)
+    st.markdown("""
+    Dimana :
+    - X = data yang akan dinormalisasi atau data asli
+    - min = nilai minimum semua data asli
+    - max = nilai maksimum semua data asli
+    """)
+    df = df.drop(columns=['Date','Open','High','Low','Close','Adj Close'])
+    #Mendefinisikan Varible X dan Y
+    X = df.drop(columns=['Volume'])
+    y = df['Volume'].values
+    df
+    X
+    df_min = X.min()
+    df_max = X.max()
     
+    #NORMALISASI NILAI X
+    scaler = MinMaxScaler()
+    #scaler.fit(features)
+    #scaler.transform(features)
+    scaled = scaler.fit_transform(X)
+    features_names = X.columns.copy()
+    #features_names.remove('label')
+    scaled_features = pd.DataFrame(scaled, columns=features_names)
 
-    df = pd.read_csv("https://raw.githubusercontent.com/RBellaApriliaDamayanti22/Datasets/main/XL-axiata.csv")
-    st.write("Dataset https://finance.yahoo.com/quote/PTXKY/history?p=PTXKY: ")
-    st.write(df)
+    st.subheader('Hasil Normalisasi Data')
+    st.write(scaled_features)
 
-with tab2:
-    st.write("Data preprocessing adalah proses yang mengubah data mentah ke dalam bentuk yang lebih mudah dipahami. Proses ini penting dilakukan karena data mentah sering kali tidak memiliki format yang teratur. Selain itu, data mining juga tidak dapat memproses data mentah, sehingga proses ini sangat penting dilakukan untuk mempermudah proses berikutnya, yakni analisis data.")
-    st.write("Data preprocessing adalah proses yang penting dilakukan guna mempermudah proses analisis data. Proses ini dapat menyeleksi data dari berbagai sumber dan menyeragamkan formatnya ke dalam satu set data.")
-    
-    scaler = st.radio(
-    "Pilih Metode Normalisasi Data : ",
-    ('Tanpa Scaler', 'MinMax Scaler'))
-    if scaler == 'Tanpa Scaler':
-        st.write("Dataset Tanpa Preprocessing : ")
-        df_new=df
-    elif scaler == 'MinMax Scaler':
-        st.write("Dataset setelah Preprocessing dengan MinMax Scaler: ")
-        scaler = MinMaxScaler()
-        df_drop_column_for_minmaxscaler=df.drop(['Date','Open','High','Low','Close','Adj Close'], axis=1)
-        df_scaler = scaler.fit_transform(df_drop_column_for_minmaxscaler)
-        df_new = df_scaler
-        
-    st.write(df_new)
+with modeling:
+    training, test = train_test_split(scaled_features,test_size=0.2, random_state=1)#Nilai X training dan Nilai X testing
+    training_label, test_label = train_test_split(y, test_size=0.2, random_state=1)#Nilai Y training dan Nilai Y testing
+    with st.form("modeling"):
+        st.subheader('Modeling')
+        st.write("Pilihlah model yang akan dilakukan pengecekkan akurasi:")
+        naive = st.checkbox('Gaussian Naive Bayes')
+        k_nn = st.checkbox('K-Nearest Neighboor')
+        destree = st.checkbox('Decission Tree')
+        submitted = st.form_submit_button("Submit")
 
-with tab3:
-    st.write("""
-    <h5>Implementation Naive Bayes</h5>
-    <br>
-    """, unsafe_allow_html=True)
-    col1,col2 = st.columns([2,2])
-    with col1:
-        a = st.number_input("Volume",0)
-        
-    submit = st.button('Prediksi')
-    if submit:
-        scaler = MinMaxScaler()
-        df_drop_column_for_minmaxscaler=df.drop(['Date','Open','High','Low','Close','Adj Close'], axis=1)
-        df_scaler = scaler.fit_transform(df_drop_column_for_minmaxscaler)
+        # NB
+        GaussianNB(priors=None)
 
-        #Train test split
-        training, test = train_test_split(df_scaler,test_size=0.3, random_state=1)#Nilai X training dan Nilai X testing
-        training_label, test_label = train_test_split(df['Volume'], test_size=0.3, random_state=1)#Nilai Y training dan Nilai Y testing    
-
-        # model
+        # Fitting Naive Bayes Classification to the Training set with linear kernel
         gaussian = GaussianNB()
-        clf = gaussian.fit(training, training_label)
-        y_pred = clf.predict(test)
+        gaussian = gaussian.fit(training, training_label)
 
-        #Evaluasi
-        akurasi = accuracy_score(test_label, y_pred)
+        # Predicting the Test set results
+        y_pred = gaussian.predict(test)
+    
+        y_compare = np.vstack((test_label,y_pred)).T
+        gaussian.predict_proba(test)
+        gaussian_akurasi = round(100 * accuracy_score(test_label, y_pred))
+        # akurasi = 10
 
-        #Inputan 
-        inputs = np.array([
-                    a
-                ])
-        df_min = df_scaler.min()
-        df_max = df_scaler.max()
-        input_norm = ((inputs - df_min) / (df_max - df_min))
-        input_norm = np.array(input_norm).reshape(1, -1)
+        #Gaussian Naive Bayes
+        # gaussian = GaussianNB()
+        # gaussian = gaussian.fit(training, training_label)
 
-        input_pred = clf.predict(input_norm)
+        # probas = gaussian.predict_proba(test)
+        # probas = probas[:,1]
+        # probas = probas.round()
 
-        st.subheader('Hasil Akurasi')
-        st.info(akurasi)
-        st.subheader('Hasil Prediksi')
-        st.write(input_pred)
+        # gaussian_akurasi = round(100 * accuracy_score(test_label,probas))
 
-        if input_pred == 0 :
-            st.write('Tidak Memenuhi ')
-        else:
-            st.write('Memenuhi')
+        #KNN
+        K=10
+        knn=KNeighborsClassifier(n_neighbors=K)
+        knn.fit(training,training_label)
+        knn_predict=knn.predict(test)
 
-        # st.subheader('Preprocessing')
-        # st.write("Case Folding :",lower_case_isi)
-        # st.write("Cleansing :",clean_symbols)
-        # st.write("Slang Word :",slang)
-        # st.write("Steaming :",stem)
+        knn_akurasi = round(100 * accuracy_score(test_label,knn_predict))
 
-        # st.subheader('Akurasi')
-        # st.info(akurasi)
+        #Decission Tree
+        dt = DecisionTreeClassifier()
+        dt.fit(training, training_label)
+        # prediction
+        dt_pred = dt.predict(test)
+        #Accuracy
+        dt_akurasi = round(100 * accuracy_score(test_label,dt_pred))
 
-        # st.subheader('Prediksi')
-        # if y_preds == "Positif":
-        #     st.success('Positive')
-        # else:
-        #     st.error('Negative')
+        if submitted :
+            if naive :
+                st.write('Model Naive Bayes accuracy score: {0:0.2f}'. format(gaussian_akurasi))
+            if k_nn :
+                st.write("Model KNN accuracy score : {0:0.2f}" . format(knn_akurasi))
+            if destree :
+                st.write("Model Decision Tree accuracy score : {0:0.2f}" . format(dt_akurasi))
+        
+        grafik = st.form_submit_button("Grafik akurasi semua model")
+        if grafik:
+            data = pd.DataFrame({
+                'Akurasi' : [gaussian_akurasi, knn_akurasi, dt_akurasi],
+                'Model' : ['Gaussian Naive Bayes', 'K-NN', 'Decission Tree'],
+            })
+
+            chart = (
+                alt.Chart(data)
+                .mark_bar()
+                .encode(
+                    alt.X("Akurasi"),
+                    alt.Y("Model"),
+                    alt.Color("Akurasi"),
+                    alt.Tooltip(["Akurasi", "Model"]),
+                )
+                .interactive()
+            )
+            st.altair_chart(chart,use_container_width=True)
+
+with implementation:
+    with st.form("my_form"):
+        st.subheader("Implementasi")
+        Weight = st.number_input('Volume) : ')
+        model = st.selectbox('Pilihlah model yang akan anda gunakan untuk melakukan prediksi?',
+                ('Gaussian Naive Bayes', 'K-NN', 'Decision Tree'))
+
+        prediksi = st.form_submit_button("Submit")
+        if prediksi:
+            inputs = np.array([a,])
+
+            df_min = X.min()
+            df_max = X.max()
+            input_norm = ((inputs - df_min) / (df_max - df_min))
+            input_norm = np.array(input_norm).reshape(1, -1)
+
+            if model == 'Gaussian Naive Bayes':
+                mod = gaussian
+            if model == 'K-NN':
+                mod = knn 
+            if model == 'Decision Tree':
+                mod = dt
+
+            input_pred = mod.predict(input_norm)
+
+
+            st.subheader('Hasil Prediksi')
+            st.write('Menggunakan Pemodelan :', model)
+
+            st.write(input_pred)
